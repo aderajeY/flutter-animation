37cd4e64c0f8b5912128c5d0f92d203e520b81fa
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-03-21 09:03:05 +0300

    animation done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AnimationPage extends StatefulWidget {
@@ -7,7 +9,23 @@ class AnimationPage extends StatefulWidget {
   State<AnimationPage> createState() => _AnimationPageState();
 }
 
-class _AnimationPageState extends State<AnimationPage> {
+class _AnimationPageState extends State<AnimationPage>
+    with SingleTickerProviderStateMixin {
+  double _radius = 100;
+  double _fontSize = 15;
+  final Tween<double> _backgroundScale = Tween<double>(begin: 0.0, end: 1.0);
+
+  AnimationController? _startIcon;
+  @override
+  void initState() {
+    super.initState();
+    _startIcon = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    );
+    _startIcon!.repeat();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,29 +34,81 @@ class _AnimationPageState extends State<AnimationPage> {
         clipBehavior: Clip.none,
         children: [
           _background(),
-          _circleAnimation(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _circleAnimation(),
+              _starIcon(),
+            ],
+          )
         ],
       ),
     ));
   }
 
   Widget _background() {
-    return Container(
-      color: Colors.blue,
+    return TweenAnimationBuilder(
+      tween: _backgroundScale,
+      duration: const Duration(seconds: 2),
+      builder: (_context, double _scale, _child) {
+        return Transform.scale(
+          scale: _scale,
+          child: _child,
+        );
+      },
+      child: Container(
+        color: Colors.blue,
+      ),
     );
   }
 
   Widget _circleAnimation() {
     return Center(
       child: GestureDetector(
-        child: Container(
-          width: 100,
-          height: 100,
+        onTap: () {
+          setState(() {
+            _radius += _radius == 200 ? -100 : 100;
+            _fontSize += _radius == 200 ? 20 : -20;
+          });
+        },
+        child: AnimatedContainer(
+          duration: const Duration(seconds: 3),
+          width: _radius,
+          height: _radius,
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(100),
+            borderRadius: BorderRadius.circular(_radius),
             color: Colors.deepPurple,
           ),
+          child: Center(
+            child: Text(
+              'Bingo!',
+              style: TextStyle(
+                color: Colors.white60,
+                fontSize: _fontSize,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget _starIcon() {
+    return AnimatedBuilder(
+      animation: _startIcon!.view,
+      builder: (_buildContext, _child) {
+        return Transform.rotate(
+          angle: _startIcon!.value * 2 * pi,
+          child: _child,
+        );
+      },
+      child: const Icon(
+        Icons.star,
+        color: Colors.white,
+        size: 200,
       ),
     );
   }
